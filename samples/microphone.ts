f6e9f9e3e2c65c1368384f6a0fd29820fcf6f85f
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-07-25 16:42:39 +0200

    Creating Microphone code example

---
+++ samples/microphone.ts
@@ -0,0 +1,22 @@
+interface Recordable {
+  handleRecording(): void;
+}
+
+class Microphone implements Recordable {
+  buildingMaterial: string[]
+  isRecording: boolean
+  recordingLevel: number
+  size: number
+
+  constructor(recordingLevel: number) {
+    this.buildingMaterial = ["metal", "plastic"]
+    this.isRecording = false
+    this.recordingLevel = recordingLevel
+    this.size = 100
+  }
+
+  handleRecording() {
+    // Fait quelque chose
+  }
+}
+
