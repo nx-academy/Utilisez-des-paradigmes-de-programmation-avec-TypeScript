a546bea517da15a26aa401f2e63ad91cc62779cd
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-07-27 10:31:30 +0200

    Creating Car example

---
+++ samples/Car.ts
@@ -0,0 +1,16 @@
+class Car {
+  brand: string
+  color: string
+  numberOfChairs: number
+  numberOfDoors: number
+  numberOfWheels: number
+
+  constructor(brand: string, color: string) {
+    this.brand = brand
+    this.color = color
+    this.numberOfChairs = 5
+    this.numberOfDoors = 5
+    this.numberOfWheels = 4
+  }
+}
+
