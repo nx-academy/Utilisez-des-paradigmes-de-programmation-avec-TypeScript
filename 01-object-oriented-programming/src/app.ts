30570b43f5cd7142402e75ef4cf38328a0954f1c
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-08-02 15:23:02 +0200

    Adding the solution

--- 01-object-oriented-programming/src/app.ts
+++ 01-object-oriented-programming/src/app.ts
@@ -1,1 +1,26 @@
+class Game {
+  gameState: string[];
+  currentPlayer: string;
+  isGameActive: boolean;
 
+  constructor() {
+    this.gameState = ["", "", "", "", "", "", "", "", ""];
+    this.currentPlayer = "X";
+    this.isGameActive = true;
+  }
+}
+
+class App {
+  game: Game;
+
+  constructor() {
+    this.game = new Game();
+  }
+
+  init(): void {
+    window.alert("Let's start the game!");
+  }
+}
+
+const app = new App();
+app.init();
