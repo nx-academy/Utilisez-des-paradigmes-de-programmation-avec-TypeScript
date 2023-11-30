8ecf7ffd76c0353ac9cb9ebd448708b9ce1dea24
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-11-30 16:03:08 +0100

    Adding result

--- 02-functional-programming/src/app.ts
+++ 02-functional-programming/src/app.ts
@@ -1,1 +1,19 @@
+function updateStatus(fn: string): void {
+  const $gameStatus = document.querySelector(".game-status");
 
+  if ($gameStatus) {
+    $gameStatus.textContent = fn;
+  }
+}
+
+function getGameStatus(player: string): string {
+  return `It's ${player}'s turn`;
+}
+
+function getWinningMessage(player: string): string {
+  return `Player ${player} has won!`;
+}
+
+function getDrawMessage(): string {
+  return "Game ended in a draw";
+}
