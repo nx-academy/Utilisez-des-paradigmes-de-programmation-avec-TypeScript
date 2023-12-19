3fafe0ba5834cf88a3c07b05f2704c1579de2af5
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-12-19 09:02:59 +0100

    Cleaning repo before recording screencasts

--- samples/array.js
+++
@@ -1,6 +0,0 @@
-const numbers = [1, 2, 3, 4]
-
-const numbersByTwo = numbers.map(number => number * 2)
-
-console.log("Notre premier tableau 'numbers'", numbers)
-console.log("Notre deuxième tableau 'numbersByTwo'", numbersByTwo)
